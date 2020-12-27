a3bae7adc6093458b8792e41151edf74e87a7740
Daniel Pourik <67840613+danielpourik@users.noreply.github.com>
2020-12-27 19:02:46 +0000

    first test

---
+++ NN.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+class Network(object):
+    def __init__(self, layers, learningRate=0.001):
+        self.layers = layers
+        self.learningRate = learningRate
+        self.weights = [np.random.normal(0.0, pow(2, -0.5),(node , synapse)) for (node , synapse) in zip(layers[:-1],layers[1:])]
+        self.s_outputs = [np.zeros(z) for z in layers[1:]]#include hidden layers outputs # this output store sigmoid output
+       
+    def softmax(self,x):
+        return np.exp(x) / np.sum(np.exp(x)) 
+    
+    def softmax_grad(self,x):
+        s = x.reshape(-1,1)
+        return np.diagflat(x) - np.dot(x, x.T)
+
+    def sigmoid(self,x):
+     
+        return (1.0 / ( 1.0 + np.exp(-x)))
+    
+    def derevative_sigmoid(self,x):
+        return self.sigmoid(x) * (1.0 - self.sigmoid(x))
+
+    def feedForward(self, inputs):
+        # output = sigmoid(wieght * inputs)
+        _inputs = inputs.copy()
+        for i in range(len(self.layers)-1):
+            temp = np.dot(_inputs,self.weights[i])
+            self.s_outputs[i]=self.sigmoid(temp)
+            _inputs = self.s_outputs[i]
+
+    def backPropagation(self, inputs, expected_output):
+        #1-last error = expected - current
+        #2-hidden_error = [weights].T*[error]
+        #3-back propagation equation:
+        #   newWeight = oldWeight + learningRate * (error * derivative_sigmoid( dot(oldWeight * prev_outputs) ) * prev_outputs.T)
+        error = expected_output - self.s_outputs[len(self.s_outputs)-1]
+
+        for i in range(len(self.s_outputs)-1, 0 , -1):
+            if(i-1>=0):
+                sigO = np.dot(self.weights[i].T,self.s_outputs[i-1])
+                prevOut = self.s_outputs[i-1]
+            else:
+                sigO = np.dot(self.weights[i].T,inputs)
+                prevOut = inputs
+            derivative = self.derevative_sigmoid(sigO)
+            hidden_error = np.dot(self.weights[i] , error)
+            self.weights[i] += self.learningRate * np.dot(np.matrix(prevOut).T,np.matrix(error * derivative))
+            error = hidden_error
+            
+    def train(self, inputs, outputs, epoch):
+        for i in range(epoch):
+            print("epoch:",i)
+            self.feedForward(inputs)
+            self.backPropagation(input,outputs)
+
+    
+    def predict(self, input):
+        self.feedForward(input)
+        return np.argmax(self.s_outputs[len(self.s_outputs)-1])
+
+
+
+
+
+        
+
+
+
+
+
---
+++ runTest.py
@@ -0,0 +1,38 @@
+import numpy as np
+import NN
+from mlxtend.data import loadlocal_mnist
+
+
+X_train, y_train = loadlocal_mnist(images_path='train-images.idx3-ubyte', 
+                                    labels_path='train-labels.idx1-ubyte')
+
+
+X_test, y_test = loadlocal_mnist(images_path='train-images.idx3-ubyte', 
+                                labels_path='train-labels.idx1-ubyte')
+
+
+output = np.array([ [1,0,0,0,0,0,0,0,0,0],
+                    [0,1,0,0,0,0,0,0,0,0],
+                    [0,0,1,0,0,0,0,0,0,0],
+                    [0,0,0,1,0,0,0,0,0,0],
+                    [0,0,0,0,1,0,0,0,0,0],
+                    [0,0,0,0,0,1,0,0,0,0],
+                    [0,0,0,0,0,0,1,0,0,0],
+                    [0,0,0,0,0,0,0,1,0,0],
+                    [0,0,0,0,0,0,0,0,1,0],
+                    [0,0,0,0,0,0,0,0,0,1]],dtype=float)
+
+net = NN.Network([784,128,10])
+for i, img in enumerate(X_train):
+    img = img / 255.0 
+    net.train(img,output[y_train[i]],epoch=20)
+
+'''
+import matplotlib.pyplot as plt
+plt.imshow(X_test[0].reshape(28,28), cmap='gray')
+plt.show()
+'''
+# it just tests 10 images, but for better performance you can implement a cost function with a higher epoch and whole test images files
+for i in range(10):
+    print("predict output:",net.predict(X_test[i]/255.0),"<-> real:",y_test[i])
+
